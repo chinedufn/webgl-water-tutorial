d5c5d465bd67b4c56bb33efc7765c2f32b8c8683
Chinedu Francis Nwafili <frankie.nwafili@gmail.com>
2018-12-31 14:44:02 -0500

    Start cleaning up water fragment shader

--- src/shader/water-fragment.glsl
+++ src/shader/water-fragment.glsl
@@ -28,6 +28,10 @@ uniform float fresnelStrength;
 vec4 shallowWaterColor =  vec4(0.0, 0.1, 0.3, 1.0);
 vec4 deepWaterColor = vec4(0.0, 0.1, 0.2, 1.0);
 
+vec3 getNormal(vec2 textureCoords);
+
+
+
 void main() {
     // Between 0 and 1
     vec2 ndc = (clipSpace.xy / clipSpace.w) / 2.0 + 0.5;
@@ -72,19 +76,13 @@ void main() {
 
     vec3 toCamera = normalize(fromFragmentToCamera);
 
-    vec4 normalMapColor = texture2D(normalMap, distortedTexCoords);
-    float makeNormalPointUpwardsMore = 2.6;
-    vec3 normal = vec3(
-      normalMapColor.r * 2.0 - 1.0,
-      normalMapColor.b * makeNormalPointUpwardsMore,
-      normalMapColor.g * 2.0 - 1.0
-    );
-    normal = normalize(normal);
+    vec3 normal = getNormal(distortedTexCoords);
 
     // Fresnel Effect. Looking at the water from above makes the water more transparent.
     float refractiveFactor = dot(toCamera, normal);
 
-    // A higher power makes the water more reflective since the refractive factor will decrease
+    // A higher fresnelStrength makes the water more reflective since the
+    // refractive factor will decrease
     refractiveFactor = pow(refractiveFactor, fresnelStrength);
 
     vec3 reflectedLight = reflect(normalize(sunlightDir), normal);
@@ -97,8 +95,18 @@ void main() {
     // Mix in a bit of blue so that it looks like water
     gl_FragColor = mix(gl_FragColor, shallowWaterColor, 0.2) + vec4(specularHighlights, 0.0);
 
-
-    // FIXME: Remove all of the alpha and water depth blending stuff and see if it makes a difference in our
-    // final scene..
     gl_FragColor.a = clamp(waterToFloor / fullOpacityDepth, 0.0, 1.0);
+}
+
+vec3 getNormal(vec2 textureCoords) {
+    vec4 normalMapColor = texture2D(normalMap, textureCoords);
+    float makeNormalPointUpwardsMore = 2.6;
+    vec3 normal = vec3(
+      normalMapColor.r * 2.0 - 1.0,
+      normalMapColor.b * makeNormalPointUpwardsMore,
+      normalMapColor.g * 2.0 - 1.0
+    );
+    normal = normalize(normal);
+
+    return normal;
 }
